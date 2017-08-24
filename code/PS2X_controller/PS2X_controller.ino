2f2117c516cac29f6344cf80e1ff1bd1afc1e17f
Pascal Gremaud <polchky@gmail.com>
2017-08-24 22:23:17 +0200

    Add serial communication to arduino controller

--- code/PS2X_controller/PS2X_controller.ino
+++ code/PS2X_controller/PS2X_controller.ino
@@ -8,102 +8,80 @@
 #define PS2_SEL        4  
 #define PS2_CLK        6  
 
-#define DELAY_MS       10
+#define DELAY_MS       20 // Delay between gamepad readings
 
 /******************************************************************
  * select modes of PS2 controller:
  *   - pressures = analog reading of push-butttons 
  *   - rumble    = motor rumbling
- * uncomment 1 of the lines for each mode selection
  ******************************************************************/
 #define pressures   false
 #define rumble      false
 
 PS2X ps2x; // create PS2 Controller Class
 
+byte sticks[] = {PSS_LX, PSS_LY, PSS_RX, PSS_RY};
+unsigned int l_buttons[] = {PSB_PAD_UP, PSB_PAD_RIGHT, PSB_PAD_DOWN, PSB_PAD_LEFT, PSB_L1, PSB_L2, PSB_L3};
+unsigned int r_buttons[] = {PSB_TRIANGLE, PSB_CIRCLE, PSB_CROSS, PSB_SQUARE, PSB_R1, PSB_R2, PSB_R3};
+unsigned int m_buttons[] = {PSB_SELECT, PSB_START};
+
+byte s_data[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; // bytes to send
+
 int error = 0;
 byte type = 0;
 byte vibrate = 0;
 
 void ps2_connect(){
-
-  error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
-
-  while(error != 0 && error != 3){
+  do{
     delay(500);
     error = ps2x.config_gamepad(PS2_CLK, PS2_CMD, PS2_SEL, PS2_DAT, pressures, rumble);
+  }while(error != 0 && error != 3);
+}
+
+void set_s_data(){
+  
+  // joysticks
+  s_data[6] = 0x00;
+  for (int i=0; i<4; i++){
+    s_data[i] = ps2x.Analog(sticks[i]) >> 1;
+    s_data[6] = s_data[6] << 1 | ps2x.Analog(sticks[i]) >> 7;
   }
+
+  // left and right buttons
+  s_data[4] = s_data[5] = 0x00;
+  for (int i=0; i<7; i++){
+    s_data[4] = s_data[4] << 1 | ps2x.Button(l_buttons[i]);
+    s_data[5] = s_data[5] << 1 | ps2x.Button(r_buttons[i]);
+  }
+
+  // middle buttons
+  s_data[6] = s_data[6] << 2 | ps2x.Button(m_buttons[0]) << 1| ps2x.Button(m_buttons[1]);
+
+  // set message starting bit
+  s_data[0] |= 1 << 7;
 }
 
 void setup(){
  
   Serial.begin(57600);
   
-  delay(300);
   ps2_connect();
 }
 
 void loop() {
-
-  //DualShock Controller
-  //ps2x.read_gamepad(false, vibrate); //read controller and set large motor to spin at 'vibrate' speed
+  
+  // read controller
   if(!ps2x.read_gamepad(false, vibrate)){
     ps2_connect();
     return;
   }
-  
-  if(ps2x.Button(PSB_START))         //will be TRUE as long as button is pressed
-    Serial.println("Start is being held");
-  if(ps2x.Button(PSB_SELECT))
-    Serial.println("Select is being held");      
 
-  if(ps2x.Button(PSB_PAD_UP)) {      //will be TRUE as long as button is pressed
-    Serial.print("Up held this hard: ");
-    Serial.println(ps2x.Analog(PSAB_PAD_UP), DEC);
+  // set and send data
+  set_s_data();
+  if(Serial.availableForWrite() >= 7){
+    for (int i=0; i<7; i++){
+      Serial.write(s_data[i]);
+    }
   }
-  if(ps2x.Button(PSB_PAD_RIGHT)){
-    Serial.print("Right held this hard: ");
-    Serial.println(ps2x.Analog(PSAB_PAD_RIGHT), DEC);
-  }
-  if(ps2x.Button(PSB_PAD_LEFT)){
-    Serial.print("LEFT held this hard: ");
-    Serial.println(ps2x.Analog(PSAB_PAD_LEFT), DEC);
-  }
-  if(ps2x.Button(PSB_PAD_DOWN)){
-    Serial.print("DOWN held this hard: ");
-    Serial.println(ps2x.Analog(PSAB_PAD_DOWN), DEC);
-  }   
-
-  vibrate = ps2x.Analog(PSAB_CROSS);  //this will set the large motor vibrate speed based on how hard you press the blue (X) button
-  if (ps2x.NewButtonState()) {        //will be TRUE if any button changes state (on to off, or off to on)
-    if(ps2x.Button(PSB_L3))
-      Serial.println("L3 pressed");
-    if(ps2x.Button(PSB_R3))
-      Serial.println("R3 pressed");
-    if(ps2x.Button(PSB_L2))
-      Serial.println("L2 pressed");
-    if(ps2x.Button(PSB_R2))
-      Serial.println("R2 pressed");
-    if(ps2x.Button(PSB_TRIANGLE))
-      Serial.println("Triangle pressed");        
-  }
-
-  if(ps2x.ButtonPressed(PSB_CIRCLE))               //will be TRUE if button was JUST pressed
-    Serial.println("Circle just pressed");
-  if(ps2x.NewButtonState(PSB_CROSS))               //will be TRUE if button was JUST pressed OR released
-    Serial.println("X just changed");
-  if(ps2x.ButtonReleased(PSB_SQUARE))              //will be TRUE if button was JUST released
-    Serial.println("Square just released");     
-
-  if(ps2x.Button(PSB_L1) || ps2x.Button(PSB_R1)) { //print stick values if either is TRUE
-    Serial.print("Stick Values:");
-    Serial.print(ps2x.Analog(PSS_LY), DEC); //Left stick, Y axis. Other options: LX, RY, RX  
-    Serial.print(",");
-    Serial.print(ps2x.Analog(PSS_LX), DEC); 
-    Serial.print(",");
-    Serial.print(ps2x.Analog(PSS_RY), DEC); 
-    Serial.print(",");
-    Serial.println(ps2x.Analog(PSS_RX), DEC); 
-  }     
   delay(DELAY_MS);  
 }
