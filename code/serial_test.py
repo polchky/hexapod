18581a1063f1af0c91af308e63b115d837d15a44
Pascal Gremaud <polchky@gmail.com>
2017-08-26 00:36:16 +0200

    Correct controller reader code

--- code/serial_test.py
+++ code/serial_test.py
@@ -2,10 +2,6 @@ import machine
 import time
 import struct
 
-ok = 0
-timeout = 0
-nope = 0
-
 sticks = ['PSS_LX', 'PSS_LY', 'PSS_RX', 'PSS_RY']
 l_buttons = ['PSB_PAD_UP', 'PSB_PAD_RIGHT', 'PSB_PAD_DOWN', 'PSB_PAD_LEFT', 'PSB_L1', 'PSB_L2', 'PSB_L3']
 r_buttons = ['PSB_TRIANGLE', 'PSB_CIRCLE', 'PSB_CROSS', 'PSB_SQUARE', 'PSB_R1', 'PSB_R2', 'PSB_R3']
@@ -22,8 +18,8 @@ def store_s_data(message):
     commands = {}
     # joysticks
     for i in range(4):
-        fb = message[6] >> 4-i & 1
-        commands[sticks[i]] = message[i] << 1 | fb
+        lb = message[6] >> 4-i & 1
+        commands[sticks[i]] = message[i] << 1 | lb
     # left and right buttons
     for i in range(7):
         commands[l_buttons[i]] = message[4] >> 6 - i & 1
@@ -36,16 +32,12 @@ def store_s_data(message):
 
 
 def read_s_data():
-    global ok
-    global nope
-    global timeout
 
     start = time.ticks_ms()
 
     # search for message start
     while(uart.any() >= 7):
         if(time.ticks_ms() - start > max_time):
-            timeout += 1
             return False
         b = struct.unpack('B', uart.read(1))[0]
         if(not b >> 7):
@@ -55,26 +47,12 @@ def read_s_data():
             uart.read(6)
             continue
         # save message
-        print("ok")
         b &= 127
         message = [b] + list(struct.unpack('BBBBBB', uart.read(6)))
         store_s_data(message)
-        ok += 1
         return True
-    nope += 1
     return False
 
 while(True):
-    a = time.ticks_ms()
     read_s_data()
     time.sleep_ms(50)
-'''
-z = 0
-while(z < 100):
-    read_s_data()
-    time.sleep_ms(20)
-    z += 1
-print(ok)
-print(nope)
-print(timeout)
-'''
