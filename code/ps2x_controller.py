ea4aab74d742088fd52627ea4e95f7be72c1fc56
Pascal Gremaud <polchky@gmail.com>
2017-09-01 22:35:24 +0200

    Transform ps2x_controller to class

--- code/ps2x_controller.py
+++ code/ps2x_controller.py
@@ -2,77 +2,70 @@ import machine
 import time
 import struct
 
-max_time_ms = 100
-baud_rate = 115200
+STICKS = ['PSS_LX', 'PSS_LY', 'PSS_RX', 'PSS_RY']
+L_BUTTONS = ['PSB_PAD_UP', 'PSB_PAD_RIGHT', 'PSB_PAD_DOWN', 'PSB_PAD_LEFT', 'PSB_L1', 'PSB_L2', 'PSB_L3']
+R_BUTTONS = ['PSB_TRIANGLE', 'PSB_CIRCLE', 'PSB_CROSS', 'PSB_SQUARE', 'PSB_R1', 'PSB_R2', 'PSB_R3']
+M_BUTTONS = ['PSB_SELECT', 'PSB_START']
+BUTTONS = L_BUTTONS + R_BUTTONS + M_BUTTONS
 
-sticks = ['PSS_LX', 'PSS_LY', 'PSS_RX', 'PSS_RY']
-l_buttons = ['PSB_PAD_UP', 'PSB_PAD_RIGHT', 'PSB_PAD_DOWN', 'PSB_PAD_LEFT', 'PSB_L1', 'PSB_L2', 'PSB_L3']
-r_buttons = ['PSB_TRIANGLE', 'PSB_CIRCLE', 'PSB_CROSS', 'PSB_SQUARE', 'PSB_R1', 'PSB_R2', 'PSB_R3']
-m_buttons = ['PSB_SELECT', 'PSB_START']
+class Controller:
+    def __init__(self, baud_rate, uart_channel = 2, max_time_ms = 50):
+        self.uart = machine.UART(2, baud_rate)
+        self.uart.init(baud_rate, bits=8, parity=None, stop=1)
+        self.max_time_ms = max_time_ms
 
-buttons = l_buttons + r_buttons + m_buttons
+        self.commands = {}
+        # init buttons state
+        for button in BUTTONS:
+            self.commands[button] = False
+        self.commands_prev = self.commands
 
-uart = machine.UART(2, baud_rate)
-uart.init(baud_rate, bits=8, parity=None, stop=1)
+    def _store_message(self, message):
+        self.commands_prev = self.commands
+        self.commands = {}
 
-commands = {}
-# init buttons state
-for button in buttons:
-    commands[button] = False
-commands_prev = commands
+        # joysticks
+        for i in range(4):
+            lb = message[6] >> 4-i & 1
+            self.commands[STICKS[i]] = message[i] << 1 | lb
+        # left and right buttons
+        for i in range(7):
+            self.commands[L_BUTTONS[i]] = message[4] >> 6 - i & 1
+            self.commands[R_BUTTONS[i]] = message[5] >> 6 - i & 1
+        # middle buttons
+        self.commands[M_BUTTONS[0]] = message[6] >> 6
+        self.commands[M_BUTTONS[1]] = message[6] >> 5 & 1
 
-def clicked(button):
-    return not commands_prev[button] and commands[button]
+    def update(self):
+        message = []
+        start = time.ticks_ms()
+        # send request byte
+        self.uart.write(bytes([0x00]))
+        while(len(message) < 7):
+            # timetout
+            if time.ticks_ms() - start > self.max_time_ms:
+                return False
+            # read incoming bytes
+            b = self.uart.read()
+            f = 'B' * len(b)
+            message += list(struct.unpack(f, b))
+            # find message start
+            while(len(message) and not message[0] >> 7):
+                del message[0]
+        # remove start bit
+        message[0] &= 127
+        self._store_message(message)
+        return True
 
-def store_data(message):
-    global commands
-    global commands_prev
-    commands_prev = commands
-    commands = {}
+    def clicked(self, button):
+        return not self.commands_prev[button] and self.commands[button]
 
-    # joysticks
-    for i in range(4):
-        lb = message[6] >> 4-i & 1
-        commands[sticks[i]] = message[i] << 1 | lb
-    # left and right buttons
-    for i in range(7):
-        commands[l_buttons[i]] = message[4] >> 6 - i & 1
-        commands[r_buttons[i]] = message[5] >> 6 - i & 1
-    # middle buttons
-    commands[m_buttons[0]] = message[6] >> 6
-    commands[m_buttons[1]] = message[6] >> 5 & 1
-
-    return
-
-def update():
-    message = []
-    start = time.ticks_ms()
-    # send request byte
-    uart.write(bytes([0x00]))
-    while(len(message) < 7):
-        # timetout
-        if time.ticks_ms() - start > max_time_ms:
-            return False
-        # read incoming bytes
-        b = uart.read()
-        f = 'B' * len(b)
-        message += list(struct.unpack(f, b))
-        # find message start
-        while(len(message) and not message[0] >> 7):
-            del message[0]
-    # remove start bit
-    message[0] &= 127
-    store_data(message)
-    #print(time.ticks_ms() - start)
-    return True
-
-def test():
-    global commands
-    # ensure keyboard interrupt did not empty the commands
-    commands = commands_prev
-    while(True):
-        if not update():
-            continue
-        for button in buttons:
-            if clicked(button):
-                print(button)
+    def test(self):
+        # ensure keyboard interrupt did not empty the commands
+        self.commands = self.commands_prev
+        while(True):
+            if not self.update():
+                continue
+            for button in BUTTONS:
+                if self.clicked(button):
+                    print(button)
